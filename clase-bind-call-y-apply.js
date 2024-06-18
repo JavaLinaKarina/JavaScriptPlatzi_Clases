bf9763bd04f902d543fe24d1ed1c635c55b6812f
Lina Karina <lina.karina.martinez@gmail.com>
2024-06-18 07:00:49 -0500

    clase de javaScript 18 de Junio 2024 parte 2

---
+++ clase-bind-call-y-apply.js
@@ -0,0 +1,14 @@
+
+const owner = "Lina"
+const address = "Calle 1ra"
+
+function dogGreeting(owner, address)
+{
+    console.log(`Hi, I am  ${this.dogName} and my owner is ${owner} and my address is ${address}`)
+}
+
+const newHouse = {
+    dogName: "Doky"
+}
+
+dogGreeting.call(newHouse, owner, address)
