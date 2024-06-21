9025b918b7df286c2d4ed557659f16d2938c12d1
Lina Karina <lina.karina.martinez@gmail.com>
2024-06-21 06:25:18 -0500

    agregando contenido de funciones

---
+++ ejercicio-practico-funciones.js
@@ -0,0 +1,25 @@
+//Create powerpuffgirl with objects
+
+function PowerpuffGirl(name, color, superPower){
+    this.color = color
+    this.name = name
+    this.superPower = superPower
+    this.isLeader = false
+
+    this.displayInfo = function(){
+        console.log(`PowerPuff Girl Information:  
+        Name: ${this.name}
+        Color: ${this.color}
+        Power: ${this.superPower}`
+    )
+    }
+}
+
+
+const blossom = new PowerpuffGirl("Lina", "Pink", "Automatizer")
+const violet = new PowerpuffGirl("Violet", "Purple", "Big team-player")
+const sad = new PowerpuffGirl("Sad", "Grey", "being blue")
+
+blossom.displayInfo()
+violet.displayInfo()
+sad.displayInfo()
