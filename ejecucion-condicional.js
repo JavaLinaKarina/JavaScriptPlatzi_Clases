8765073efc675640b08a7630a4c7d96e93193e93
Lina Karina <lina.karina.martinez@gmail.com>
2024-07-01 04:53:36 -0500

    agregando clase condicional

---
+++ ejecucion-condicional.js
@@ -0,0 +1,21 @@
+/*let message
+
+const age = 16
+
+if(age >=  18){
+    message = "is major of age, you can pass"
+}
+else{
+    message = "Not is major of age, you can not pass"
+}
+
+console.log(message)*/
+
+/*condicion ? true : false */ 
+
+
+const age = 10
+const message = age >=  18 ? "is major of age, you can pass" : "Not is major of age, you can not pass"
+console.log(message)
+
+
